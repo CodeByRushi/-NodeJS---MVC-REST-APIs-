2be10aef8312c3b8504512e0a3548e9ae8ebf7e3
Rushi <rushikeshshetye40@gmail.com>
2023-01-18 10:39:28 +0530

    Styles are added

--- 2.WorkingWithExpress/WorkingWithRouter/routes/admin.js
+++ 2.WorkingWithExpress/WorkingWithRouter/routes/admin.js
@@ -2,13 +2,13 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
-router.get('/add-product', (req,res,next)=>{
-    console.log(path.join(__dirname,'../','view','add-product.html'));
-    res.sendFile(path.join(__dirname,'../','view','add-product.html'));
+router.get('/add-product', (req, res, next) => {
+    // console.log(path.join(__dirname, '../', 'view', 'add-product.html'));
+    res.sendFile(path.join(__dirname, '../', 'view', 'add-product.html'));
     // res.send('<h1>HI from my admin</h1>');
 });
-router.post('/add-product',(req,res,next)=>{
-    console.log(req.body.title);
+router.post('/add-product', (req, res, next) => {
+    console.log("Data Entered is :", req.body.title);
     // res.send('<h1>HI from my admin -> add-product</h1>');
     res.redirect('/');
 });
